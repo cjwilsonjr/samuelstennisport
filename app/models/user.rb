f5ec7919af3c24bee81dcb872aebe5c18829553b
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-08 13:04:36 -0400

    add vaildations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+
+  validates :first_name, presence: true,
+                         length: { minimum: 3, maximum: 25 }
+  validates :last_name,  presence: true,
+                         length: { minimum: 3, maximum: 25 }
+  validates :email,      uniqueness: true,
+                         format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: { minimum: 6 }
   has_secure_password
 end
