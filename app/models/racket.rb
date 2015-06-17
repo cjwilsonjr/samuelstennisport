1ed3365c692d49554715865172841de08156d826
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-16 22:59:19 -0500

    Add skeleton of method to alter last_string_change date.

--- app/models/racket.rb
+++ app/models/racket.rb
@@ -1,4 +1,13 @@
 class Racket < ActiveRecord::Base
   belongs_to :owner, class_name: "Customer"
   has_many :stringsets
+
+  def last_string_change
+    # if last_string_change
+      # if last_string_change
+    #   puts "HELLO"
+    # else
+    #   puts "This is"
+    # end
+  end
 end
