8304c5a06ed171d5f9d4be04c7d5553279a341e8
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-18 13:04:16 -0500

    Add months_between_string_change and string_Changes_per_year to determine how often a customer plays.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ActiveRecord::Base
   has_many :rackets, foreign_key: :owner_id
   has_many :stringsets, through: :rackets
+
+  def months_between_string_change
+    12 / play_per_week
+  end
+
+  def string_changes_per_year
+    12 / months_between_string_change
+  end
 end
