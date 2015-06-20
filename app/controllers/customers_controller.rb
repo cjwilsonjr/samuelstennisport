3eae1595d9e6821b219011c603945554a04ddcd0
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-20 16:39:09 -0500

    Delete commented out code in customers controller.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,6 @@ class CustomersController < ApplicationController
 
   def index
     @customers = Customer.all
-    # @customer = Customer.new
     @firstnames = @customers.map { |c| c.first_name }
     @lastnames = @customers.map { |c| c.last_name }
     @emails = @customers.map { |c| c.email }
@@ -28,17 +27,7 @@ class CustomersController < ApplicationController
 
   def show
     @customer = Customer.find_by_id(params[:id])
-    puts "CUSTOMER"
-    puts @customer
-    puts "BASDASD"
     @rackets = Racket.all
-    # @racket = Racket.new
-    # if @racket.save
-    #   @customer.rackets << @racket
-    #   redirect_to @racket
-    # else
-    #   render "rackets/form"
-    # end
   end
 
   private
