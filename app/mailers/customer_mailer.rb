b9b070d4f43e5b092b90153e15939eae92e4ec7b
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-20 19:27:59 -0500

    Add action mailer settings to allow emails in development environemnt.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,9 +1,8 @@
 class CustomerMailer < ApplicationMailer
-  default from: 'christopher.wilson.jr@gmail.com'
 
   def customer_email(customer)
     @customer = customer
-    @url = 'http://samuelstennisport.herokuapp.com/customers'
-    mail(to: @customer.email, subject: 'Welcome to the customer page')
+    @url = "http://samuelstennisport.herokuapp.com/customers"
+    mail(to: @customer.email, subject: "Welcome to the customer page")
   end
 end
