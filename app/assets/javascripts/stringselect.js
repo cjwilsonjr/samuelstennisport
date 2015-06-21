c9e3861196469c510548bd62655824fb88a95b99
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-21 10:51:37 -0500

    Add JS for dynamic select form for adding strings.

--- app/assets/javascripts/stringselect.js
+++ app/assets/javascripts/stringselect.js
@@ -9,36 +9,11 @@ $(document).ready(function(){
       data: {brand_type: $("#stringset_brand option:selected").val()}
       })
       .done(function(response){
-        // var r = $.parseJSON(response);
-        // console.log(r.descriptions);
-        // $("#stringset_description").empty().append(response);
-
+        var descriptionSelect = $("#stringset_description");
+        descriptionSelect.empty();
+        for (var i = 0; i < response.length; i++){
+          descriptionSelect.append('<option value=' + response[i] +'>' + response[i] + '</option>');
+        }
       });
     });
   });
-//   $(document).on("submit", "form", function(event){
-//     event.preventDefault();
-//     console.log($(this).serialize());
-//     var data = $(this).serialize();
-//     var url = $("form").attr("action");
-
-//     $.ajax({
-//       url: url,
-//       data: data,
-//       method: "POST"
-//     });
-
-//   });
-// //   $(document).on("change", "#stringset_brand", function(event){
-// //     event.preventDefault();
-// //     var url = $("form").attr("action");
-// //     // $("body").css("background", "red");
-// //     $.ajax({
-// //       url: "stringsets/update_string_brands",
-// //       data: $("#stringset_brand").val()
-// //     }).done(function(response){
-// //       console.log(response);
-// //     });
-// //   });
-
-// });
