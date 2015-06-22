59e1ba63c3219c28787ce83d7f3c7ad8383f0757
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-22 14:43:41 -0500

    Delete unnecessary controller calucation for custoemr names and emails.  Add presence validation to customers.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,9 +2,6 @@ class CustomersController < ApplicationController
 
   def index
     @customers = Customer.all
-    @firstnames = @customers.map { |c| c.first_name }
-    @lastnames = @customers.map { |c| c.last_name }
-    @emails = @customers.map { |c| c.email }
   end
 
   def new
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ActiveRecord::Base
   has_many :rackets, foreign_key: :owner_id
   has_many :stringsets, through: :rackets
 
+  validates :first_name, :last_name, :email, presence: true
+
   def months_between_string_change
     12 / play_per_week
   end
