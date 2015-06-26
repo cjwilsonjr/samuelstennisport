4ebc1adf6d154dcbb89b974244c990c9f997ccbf
CJ Wilson <christopher.wilson.jr@gmail.com>
2015-06-26 10:05:56 -0500

    Add one_week customer mailer method to deliver one week before string sare due.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -8,9 +8,10 @@ class CustomerMailer < ApplicationMailer
     mail(to: @customer.email, subject: "#{@racket.brand} Racket String Change Due Date")
   end
 
-  def reminder_email(customer, racket)
+  def one_week_reminder_email(customer, racket)
     @customer = customer
     @racket = racket
+    @stringset = racket.stringsets[-1]
     @url = "mailto:tennisport@aol.com"
     mail(to: @customer.email, subject: "Reminder: #{@racket.brand} Racket Needs New Strings")
   end
@@ -18,6 +19,7 @@ class CustomerMailer < ApplicationMailer
   def next_day_reminder_email(customer, racket)
     @customer = customer
     @racket = racket
+    @stringset = racket.stringsets[-1]
     @url = "mailto:tennisport@aol.com"
     mail(to: @customer.email, subject: "Reminder: #{@racket.brand} Racket Needs New Strings Tomorrow")
   end
